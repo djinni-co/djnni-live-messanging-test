80b52e89d5c06bf6023a618848f86dfd41b45e28
nikolay <mykola.starikov@gmail.com>
2024-05-22 10:37:21 +0300

    docker fixes

--- app/project/settings.py
+++ app/project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',  # Use the service name from docker-compose.yml
+        'HOST': os.getenv('POSTGRES_HOST'),  # Use the service name from docker-compose.yml
         'PORT': '5432',
     }
 }
